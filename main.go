5b6783f8c50b889b85945f37cd31f3badff0308f
yormansj97 <yormansjhr@gmail.com>
2025-02-04 11:44:53 -0600

    feat: implemented CI

--- main.go
+++ main.go
@@ -26,12 +26,16 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Definir la ruta de la raíz
+	http.HandleFunc("/", homePage)  // Redirige a homePage
 
+	// Rutas para el resto de las páginas
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
 
+	// Iniciar el servidor
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatal(err)
